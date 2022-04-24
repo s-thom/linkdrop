eb79e15019424b524581000ca4757c0940322719
Stuart Thomson <me@sthom.kiwi>
2022-04-24 14:37:41 +1200

    Update cypress smoke tests

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -17,32 +17,42 @@ describe("smoke tests", () => {
 
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
     cy.findByLabelText(/password/i).type(loginForm.password);
-    cy.findByRole("button", { name: /create account/i }).click();
+    cy.findByRole("button", { name: /sign up/i }).click();
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
+    cy.findByRole("link", { name: /your links/i }).click();
+    cy.findByRole("button", { name: /log out/i }).click();
     cy.findByRole("link", { name: /log in/i });
   });
 
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
+  it("should allow you to save a link", () => {
+    const testLink = {
+      url: `https://example.com#${faker.word.noun()}`,
+      description: faker.lorem.sentences(1),
+      tags: "foo+bar,baz wibble+wobble,wubble",
     };
     cy.login();
     cy.visit("/");
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByText("No notes yet");
+    cy.findByRole("link", { name: /Your links/i }).click();
+    cy.findByText("You haven't added any links yet.");
 
-    cy.findByRole("link", { name: /\+ new note/i }).click();
+    cy.findAllByRole("link", { name: /New link/i })
+      .first()
+      .click();
 
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
+    cy.findByRole("textbox", { name: /URL/i }).type(testLink.url);
+    cy.findByRole("textbox", { name: /Description/i }).type(
+      testLink.description
+    );
+    cy.findByRole("textbox", { name: /Tags/i }).type(testLink.tags);
     cy.findByRole("button", { name: /save/i }).click();
 
-    cy.findByRole("button", { name: /delete/i }).click();
+    cy.findByRole("link", { name: testLink.url });
+    cy.findByText(testLink.description);
 
-    cy.findByText("No notes yet");
+    // TODO: editing and deleting
+    // cy.findByRole("button", { name: /delete/i }).click();
+
+    // cy.findByText("No notes yet");
   });
 });
