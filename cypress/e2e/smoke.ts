b03c964602d7c277c6e279892087a89e63200940
Stuart Thomson <me@sthom.kiwi>
2022-04-25 17:48:15 +1200

    Fix cypress tests flakiness in GH actions

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -20,8 +20,11 @@ describe("smoke tests", () => {
     cy.findByRole("button", { name: /sign up/i }).click();
 
     cy.findByRole("link", { name: /your links/i }).click();
+    cy.findByRole("textbox", { name: /enter tags/i });
+
     cy.findByRole("link", { name: /settings/i }).click();
     cy.findByRole("button", { name: /log out/i }).click();
+
     cy.findByRole("link", { name: /log in/i });
   });
 
