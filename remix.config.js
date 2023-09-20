a897dd56719a0cfadaf3007cb83a840a97d2ce46
Stuart Thomson <me@sthom.kiwi>
2023-09-21 11:37:01 +1200

    Fix incorrect module format for server runtime

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,7 @@ const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverModuleFormat: "cjs",
 
   routes(defineRoutes) {
     // uses the v1 convention, works in v1.15+ and v2
